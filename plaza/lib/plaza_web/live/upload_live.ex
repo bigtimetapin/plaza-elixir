6c17339ecd36e2efcc391778f5c3687dfe5c4e58
alex <aabinaei@gmail.com>
2023-04-09 04:42:12 -0700

    upload-live :step 1 & 2 mock

--- plaza/lib/plaza_web/live/upload_live.ex
+++ plaza/lib/plaza_web/live/upload_live.ex
@@ -6,14 +6,83 @@ defmodule PlazaWeb.UploadLive do
       socket
       |> assign(:page_title, "Upload")
       |> assign(:header, :upload)
+      |> assign(:step, 1)
 
     {:ok, socket}
   end
 
-  def render(assigns) do
+  def handle_event("step", %{"step" => "2"}, socket) do
+    socket =
+      socket
+      |> assign(:step, 2)
+
+    {:noreply, socket}
+  end
+
+  def render(%{step: 1} = assigns) do
+    ~H"""
+    <.body>
+      <:center>
+        <.one />
+      </:center>
+    </.body>
+    """
+  end
+
+  def render(%{step: 2} = assigns) do
+    ~H"""
+    <div style="margin-top: 200px;">
+      <.body>
+        <:center>
+          <.two />
+        </:center>
+      </.body>
+    </div>
+    """
+  end
+
+  slot :center, required: true
+  attr :rest, :global
+
+  defp body(assigns) do
+    ~H"""
+    <div class="columns">
+      <div class="column is-2"></div>
+      <div class="column has-font-3 is-size-8">
+        <%= render_slot(@center) %>
+      </div>
+    </div>
+    """
+  end
+
+  defp one(assigns) do
     ~H"""
     <div>
-      Upload Stuff
+      <button
+        style="width: 750px; height: 300px; border: 2px solid gray;"
+        phx-click="step"
+        phx-value-step="2"
+        class="has-font-3"
+      >
+        arraste seu arquivo aqui
+      </button>
+    </div>
+    """
+  end
+
+  defp two(assigns) do
+    ~H"""
+    <div>
+      <div style="width: 750px; height: 300px; border: 2px solid gray;" class="mb-medium">
+        <div style="display: flex; justify-content: center; align-items: center; height: 100%;">
+          arquivo enviado arte101.png
+        </div>
+      </div>
+      <div style="width: 750px; height: 300px; border: 2px solid gray;">
+        <div style="display: flex; justify-content: center; align-items: center; height: 100%;">
+          quantas cores tem sua estampa?
+        </div>
+      </div>
     </div>
     """
   end
