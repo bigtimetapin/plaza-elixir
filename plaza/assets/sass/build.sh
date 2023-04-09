07418301b7b41dc2da6bfc0d7e9bd1cde059c55e
alex <aabinaei@gmail.com>
2023-04-09 01:16:53 -0700

    tailwind rel path for treeshaking

--- plaza/assets/sass/build.sh
+++ plaza/assets/sass/build.sh
@@ -6,8 +6,4 @@ node-sass --output-style expanded --include-path=node_modules/bulma custom.scss 
 
 # run tailwind build
 echo "building tailwind assets"
-tailwindcss -c ../tailwind.config.js -i ../css/app.css -i ../css/custom.css -o ../../priv/static/assets/app.css
-
-echo "syncing with elixir priv"
-# copy to priv (just incase elixir watcher picks it up)
-cp ./../css/custom.css ./../../priv/static/assets/
+cd ..; tailwindcss --config tailwind.config.js --input css/app.css --output ../priv/static/assets/app.css
