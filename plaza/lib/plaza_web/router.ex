efde320493ed62e2728bcbb56275c1a917baf162
alex <aabinaei@gmail.com>
2023-04-09 01:22:34 -0700

    upload-live --> require-auth

--- plaza/lib/plaza_web/router.ex
+++ plaza/lib/plaza_web/router.ex
@@ -21,7 +21,6 @@ defmodule PlazaWeb.Router do
     pipe_through :browser
 
     live "/", LandingLive
-    live "/upload", UploadLive
   end
 
   # Other scopes may use custom stacks.
@@ -69,6 +68,7 @@ defmodule PlazaWeb.Router do
       on_mount: [{PlazaWeb.UserAuth, :ensure_authenticated}] do
       live "/users/settings", UserSettingsLive, :edit
       live "/users/settings/confirm_email/:token", UserSettingsLive, :confirm_email
+      live "/upload", UploadLive
     end
   end
 
