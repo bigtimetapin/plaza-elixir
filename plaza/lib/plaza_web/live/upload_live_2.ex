2cbdc9aabf70e579b62ad18360a5a207dd0f588e
alex <aabinaei@gmail.com>
2023-07-20 08:59:12 -0700

    upload landing B

--- plaza/lib/plaza_web/live/upload_live_2.ex
+++ plaza/lib/plaza_web/live/upload_live_2.ex
@@ -13,6 +13,23 @@ defmodule PlazaWeb.UploadLive2 do
   end
 
   @impl Phoenix.LiveView
+  def handle_event("step", %{"step" => "2"}, socket) do
+    socket =
+      socket
+      |> assign(:step, 2)
+
+    {:noreply, socket}
+  end
+
+  def handle_event("step", %{"step" => "3"}, socket) do
+    socket =
+      socket
+      |> assign(:step, 3)
+
+    {:noreply, socket}
+  end
+
+  @impl Phoenix.LiveView
   def render(%{step: 1} = assigns) do
     ~H"""
     <div class="has-font-3 is-size-4" style="text-align: center; margin-top: 125px;">
@@ -40,11 +57,55 @@ defmodule PlazaWeb.UploadLive2 do
         E se não vender tudo bem, ninguém perde nada.
       </div>
     </div>
-    <div style="display: flex; justify-content: center; margin-top: 85px; position: relative; left: 73px; margin-bottom: 75px;">
-      <img src="svg/yellow-ellipse.svg" />
-      <div class="has-font-3 is-size-4" style="position: relative; right: 184px; top: 21px;">
-        Criar Produto
+    <div style="display: flex; justify-content: center; margin-top: 85px; margin-bottom: 75px;">
+      <button phx-click="step" phx-value-step="2">
+        <img src="svg/yellow-ellipse.svg" />
+        <div class="has-font-3 is-size-4" style="position: relative; bottom: 79px;">
+          Criar Produto
+        </div>
+      </button>
+    </div>
+    """
+  end
+
+  def render(%{step: 2} = assigns) do
+    ~H"""
+    <div class="has-font-3 is-size-4" style="text-align: center; margin-top: 150px;">
+      <div class="mb-xsmall">
+        Tem mais um detalhe importante:
       </div>
+      <div class="mb-xsmall">
+        <div>
+          Seu produto vai ficar disponível na nossa loja por tempo limitado.
+        </div>
+        <div style="position: relative; bottom: 15px;">
+          Você pode decidir se sua campanha de vendas ficará disponível por 7, 14, 21 ou 30 dias.
+        </div>
+      </div>
+      <div>
+        <div>
+          Após o final do prazo de vendas seu produto não estará mais disponível para encomendas.
+        </div>
+        <div style="position: relative; bottom: 15px;">
+          Você tem liberdade de republicar o produto por mais períodos de até 30 dias novamente se quiser.
+        </div>
+      </div>
+    </div>
+    <div style="display: flex; justify-content: center; margin-top: 85px; margin-bottom: 50px;">
+      <button phx-click="step" phx-value-step="3">
+        <img src="svg/yellow-ellipse.svg" />
+        <div class="has-font-3 is-size-4" style="position: relative; bottom: 79px;">
+          Ok
+        </div>
+      </button>
+    </div>
+    """
+  end
+
+  def render(%{step: 3} = assigns) do
+    ~H"""
+    <div>
+      step 3
     </div>
     """
   end
