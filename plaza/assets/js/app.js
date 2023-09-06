cfd839513258b65d17a9d018e6ab4fdf7bee18d3
alex <aabinaei@gmail.com>
2023-09-06 14:39:15 -0700

    dropped mercado-pago js assets

--- plaza/assets/js/app.js
+++ plaza/assets/js/app.js
@@ -21,22 +21,12 @@ import "phoenix_html";
 import { Socket } from "phoenix";
 import { LiveSocket } from "phoenix_live_view";
 import topbar from "../vendor/topbar";
-import { renderCardPaymentBrick, renderStatusScreenBrick } from "../vendor/mercado-pago";
+
 
 // csrf token 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content");
 // hooks 
 let Hooks = {};
-Hooks.MercadoPagoHook = {
-  mounted() {
-    renderCardPaymentBrick(
-      (cardFormData) => this.pushEvent("from-js-to-phx", { foo: "bar", card_form_data: cardFormData }, (reply, ref) => {
-        renderStatusScreenBrick(reply.payment_id);
-        console.log(reply)
-      })
-    );
-  }
-};
 // live socket 
 let liveSocket = new LiveSocket("/live", Socket, { params: { _csrf_token: csrfToken }, hooks: Hooks });
 
