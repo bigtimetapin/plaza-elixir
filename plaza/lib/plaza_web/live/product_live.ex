a223eb3bede842b3c9a82fd92c73c48120f6c032
alex <aabinaei@gmail.com>
2023-10-07 17:08:33 -0700

    fetch shipping quote

--- plaza/lib/plaza_web/live/product_live.ex
+++ plaza/lib/plaza_web/live/product_live.ex
@@ -3,6 +3,7 @@ defmodule PlazaWeb.ProductLive do
 
   alias Plaza.Accounts
   alias Plaza.Accounts.Seller
+  alias Plaza.Dimona
   alias Plaza.Products
   alias PlazaWeb.ProductComponent
 
@@ -13,7 +14,7 @@ defmodule PlazaWeb.ProductLive do
         socket
       else
         socket
-        |> assign(product: nil)
+        |> assign(waiting: true)
       end
 
     {:ok, socket}
@@ -43,31 +44,112 @@ defmodule PlazaWeb.ProductLive do
         socket
         |> assign(seller: seller)
         |> assign(product: product)
+        |> assign(step: 1)
+        |> assign(waiting: false)
       else
         socket
       end
-
-    IO.inspect(socket.assigns)
 
     {:noreply, socket}
   end
 
   @impl Phoenix.LiveView
+  def handle_event("step", %{"step" => "2"}, socket) do
+    socket =
+      socket
+      |> assign(step: 2)
+      |> assign(cep: nil)
+
+    {:noreply, socket}
+  end
+
+  def handle_event("change-cep-input", %{"cep" => cep}, socket) do
+    cep =
+      case cep do
+        "" ->
+          nil
+
+        nes ->
+          nes
+      end
+
+    socket =
+      socket
+      |> assign(cep: cep)
+
+    {:noreply, socket}
+  end
+
+  def handle_event("submit-cep-input", _params, socket) do
+    params = %{"zipcode" => socket.assigns.cep}
+    result = Dimona.Requests.Shipping.post(params)
+    {:noreply, socket}
+  end
+
+  @impl Phoenix.LiveView
+  def render(%{waiting: true} = assigns) do
+    ~H"""
+    <div style="margin-top: 150px;">
+      <div style="display: flex; justify-content: center;">
+        waiting
+      </div>
+    </div>
+    """
+  end
+
   def render(%{product: nil} = assigns) do
     ~H"""
-    <div class="mx-large">
-      <div>
+    <div>
+      <div style="display: flex; justify-content: center;">
         product does not exist
       </div>
     </div>
     """
   end
 
-  def render(%{product: product} = assigns) do
+  def render(%{product: product, step: 1} = assigns) do
     ~H"""
-    <div class="mx-large">
+    <div class="has-font-3" style="font-size: 34px;">
       <div>
         <ProductComponent.product product={product} />
+      </div>
+      <div>
+        <button phx-click="step" phx-value-step="2">
+          purchase
+        </button>
+      </div>
+    </div>
+    """
+  end
+
+  def render(%{product: product, step: 2} = assigns) do
+    ~H"""
+    <div class="has-font-3" style="font-size: 34px;">
+      <div>
+        <ProductComponent.product product={product} />
+      </div>
+      <div>
+        <div>
+          we need your cep to figure out shipping costs
+        </div>
+        <div>
+          <form>
+            <.input
+              type="text"
+              name="cep"
+              value={@cep}
+              placeholder="cep"
+              class="text-input-1"
+              phx-change="change-cep-input"
+            >
+            </.input>
+          </form>
+        </div>
+        <div>
+          <button disabled={!@cep} phx-click="submit-cep-input" style={if !@cep, do: "opacity: 50%;"}>
+            submit
+          </button>
+        </div>
       </div>
     </div>
     """
