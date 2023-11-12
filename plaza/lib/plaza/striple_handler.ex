4a2bb274ff8fef4c5a179f0db70818631f21fe3b
alex <aabinaei@gmail.com>
2023-11-12 14:01:38 -0800

    Code TLC.

--- plaza/lib/plaza/striple_handler.ex
+++ plaza/lib/plaza/striple_handler.ex
@@ -2,25 +2,9 @@ defmodule Plaza.StripeHandler do
   @behaviour Stripe.WebhookHandler
 
   alias Plaza.Purchases
+  alias Plaza.Accounts.UserNotifier
 
-  ## @impl true
-  ## def handle_event(%Stripe.Event{type: "payment_intent.succeeded"} = event) do
-  ##   IO.inspect(event)
-
-  ##   ## IO.inspect(stripe_session)
-  ##   ## purchase_id = stripe_session.client_reference_id
-  ##   ## {:ok, payment_intent} = Stripe.PaymentIntent.retrieve(stripe_session.payment_intent, %{})
-  ##   ## payment_status = Purchases.normalize_payment_status(payment_intent.status)
-
-  ##   ## Phoenix.PubSub.broadcast(
-  ##   ##   Plaza.PubSub,
-  ##   ##   "payment-status-#{purchase_id}",
-  ##   ##   {:payment_status, payment_status}
-  ##   ## )
-  ##   :ok
-  ## end
-
-  # Return HTTP 200 for unhandled events
+  @impl true
   def handle_event(%Stripe.Event{type: "payment_intent.succeeded", data: data}) do
     %{object: %{metadata: %{"purchase_id" => purchase_id}}} = data
     IO.inspect(purchase_id)
@@ -33,6 +17,10 @@ defmodule Plaza.StripeHandler do
 
     purchase = Purchases.get!(purchase_id)
     IO.inspect(purchase)
+
+    response = UserNotifier.deliver_quick(%{email: purchase.email})
+
+    IO.inspect(response)
 
     :ok
   end
