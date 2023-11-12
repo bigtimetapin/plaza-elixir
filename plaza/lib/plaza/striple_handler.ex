317d3b81abf8fe82475e8f1d3d776bf3f92e6138
alex <aabinaei@gmail.com>
2023-11-12 08:44:24 -0800

    listen for payment_intent.success and pubsub to view for redundancy

--- plaza/lib/plaza/striple_handler.ex
+++ plaza/lib/plaza/striple_handler.ex
@@ -3,33 +3,37 @@ defmodule Plaza.StripeHandler do
 
   alias Plaza.Purchases
 
-  @impl true
-  def handle_event(%Stripe.Event{
-        type: "checkout.session.completed",
-        data: %{object: stripe_session}
-      }) do
-    ## IO.inspect(stripe_session)
-    ## purchase_id = stripe_session.client_reference_id
-    ## {:ok, payment_intent} = Stripe.PaymentIntent.retrieve(stripe_session.payment_intent, %{})
-    ## payment_status = Purchases.normalize_payment_status(payment_intent.status)
+  ## @impl true
+  ## def handle_event(%Stripe.Event{type: "payment_intent.succeeded"} = event) do
+  ##   IO.inspect(event)
 
-    ## Phoenix.PubSub.broadcast(
-    ##   Plaza.PubSub,
-    ##   "payment-status-#{purchase_id}",
-    ##   {:payment_status, payment_status}
-    ## )
+  ##   ## IO.inspect(stripe_session)
+  ##   ## purchase_id = stripe_session.client_reference_id
+  ##   ## {:ok, payment_intent} = Stripe.PaymentIntent.retrieve(stripe_session.payment_intent, %{})
+  ##   ## payment_status = Purchases.normalize_payment_status(payment_intent.status)
 
-    :ok
-  end
-
-  def handle_event(%Stripe.Event{type: "payment_intent.succeeded"} = event) do
-    IO.inspect(event)
-    :ok
-  end
+  ##   ## Phoenix.PubSub.broadcast(
+  ##   ##   Plaza.PubSub,
+  ##   ##   "payment-status-#{purchase_id}",
+  ##   ##   {:payment_status, payment_status}
+  ##   ## )
+  ##   :ok
+  ## end
 
   # Return HTTP 200 for unhandled events
-  def handle_event(%Stripe.Event{type: type}) do
-    IO.inspect(type)
+  def handle_event(%Stripe.Event{type: "payment_intent.succeeded", data: data}) do
+    %{object: %{metadata: %{"purchase_id" => purchase_id}}} = data
+    IO.inspect(purchase_id)
+
+    Phoenix.PubSub.broadcast(
+      Plaza.PubSub,
+      "payment-status-#{purchase_id}",
+      {:payment_status, "succeeded"}
+    )
+
+    purchase = Purchases.get!(purchase_id)
+    IO.inspect(purchase)
+
     :ok
   end
 end
