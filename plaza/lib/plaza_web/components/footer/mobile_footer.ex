9b810ad1f0132b0170daaf4c2dfc08978e095b53
alex <aabinaei@gmail.com>
2024-02-20 02:55:44 -0800

    footer resizing

--- plaza/lib/plaza_web/components/footer/mobile_footer.ex
+++ plaza/lib/plaza_web/components/footer/mobile_footer.ex
@@ -5,27 +5,27 @@ defmodule PlazaWeb.MobileFooter do
   def render(assigns) do
     ~H"""
     <div class="has-font-3" style="display: flex; justify-content: center;">
-      <div style="margin-left: 10px; margin-right: 10px; margin-top: 50px;">
-        <div style="font-size: 32px; text-align: center;" class="has-font-1">
+      <div style="margin-left: 10px; margin-right: 10px; margin-top: 75px;">
+        <div style="font-size: 30px; text-align: center;" class="has-font-1">
           Conta
         </div>
-        <div style="text-decoration: underline; text-align: center; font-size: 28px; margin-bottom: 50px;">
+        <div style="text-decoration: underline; text-align: center; font-size: 26px; margin-bottom: 75px;">
           <.link navigate="/my-account">
             Acessar Conta
           </.link>
         </div>
-        <div style="font-size: 32px; text-align: center;" class="has-font-1">
+        <div style="font-size: 30px; text-align: center;" class="has-font-1">
           Sobre
         </div>
-        <div style="text-decoration: underline; text-align: center; font-size: 28px; margin-bottom: 50px;">
+        <div style="text-decoration: underline; text-align: center; font-size: 26px; margin-bottom: 75px;">
           <a href="mailto:admin@plazaaaaa.com">
             Contato
           </a>
         </div>
-        <div style="display: flex; justify-content: center; margin-bottom: 100px;">
+        <div style="display: flex; justify-content: center; margin-bottom: 75px; margin-left: 25px; margin-right: 25px;">
           <img src="/svg/footer-left-02.svg" />
         </div>
-        <div style="font-size: 22px; line-height: 24px; display: flex; justify-content: center; margin-bottom: 100px;">
+        <div style="font-size: 22px; line-height: 24px; display: flex; justify-content: center; margin-bottom: 75px; margin-left: 10px;">
           <div style="width: 325px; ">
             Plaza é uma plataforma de integração de serviços e pagamentos automatizados para artistas e criadores,
             entre em contato para saber mais, trabalhar conosco ou qualquer dúvida.
@@ -34,7 +34,7 @@ defmodule PlazaWeb.MobileFooter do
             </a>
           </div>
         </div>
-        <div style="margin-bottom: 100px;">
+        <div style="margin-bottom: 75px; display: flex; justify-content: center; margin-right: 30px;">
           <img src="/svg/footer-right-02.svg" />
         </div>
       </div>
