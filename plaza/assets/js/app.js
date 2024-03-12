5cd2f2228de48f2da27573679bfece01d20fb1ad
alex <aabinaei@gmail.com>
2024-03-12 07:07:27 -0700

    dropped topbar loader

--- plaza/assets/js/app.js
+++ plaza/assets/js/app.js
@@ -20,7 +20,7 @@ import "phoenix_html";
 // Establish Phoenix Socket and LiveView configuration.
 import { Socket } from "phoenix";
 import { LiveSocket } from "phoenix_live_view";
-import topbar from "../vendor/topbar";
+// import topbar from "../vendor/topbar";
 import "jimp";
 
 // csrf token 
@@ -278,10 +278,10 @@ async function jimpToPng(jimp, fileName) {
 // live socket 
 let liveSocket = new LiveSocket("/live", Socket, { params: { _csrf_token: csrfToken }, hooks: Hooks });
 
-// Show progress bar on live navigation and form submits
-topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" });
-window.addEventListener("phx:page-loading-start", _info => topbar.show(300));
-window.addEventListener("phx:page-loading-stop", _info => topbar.hide());
+// // Show progress bar on live navigation and form submits
+// topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" });
+// window.addEventListener("phx:page-loading-start", _info => topbar.show(300));
+// window.addEventListener("phx:page-loading-stop", _info => topbar.hide());
 
 // connect if there are any LiveViews on the page
 liveSocket.connect();
